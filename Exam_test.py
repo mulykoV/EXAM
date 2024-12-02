c66feccbda2ea4894f0f332fcb35a7266fa4eee4
mulykoV <144117863+mulykoV@users.noreply.github.com>
2024-12-02 16:32:16 +0200

    Update Exam_test.py

--- Exam_test.py
+++ Exam_test.py
@@ -2,6 +2,6 @@ import pytest
 from Exam import nth_term_arithmetic_progression
 
 def test_nth_term_arithmetic_progression():
-    assert nth_term_arithmetic_progression(5, 2, 1) == 5
+    assert nth_term_arithmetic_progression(5, 2, 1) == -1
     assert nth_term_arithmetic_progression(5, 2, 2) == 7
     assert nth_term_arithmetic_progression(5, 2, 3) == 9
