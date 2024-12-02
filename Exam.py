5a53b0a2ce6bbe4543b8f9c87f28e468300fe16e
mulykoV <144117863+mulykoV@users.noreply.github.com>
2024-12-02 15:28:36 +0200

    Update Exam.py

--- Exam.py
+++ Exam.py
@@ -1,12 +1,9 @@
 def nth_term_arithmetic_progression(a, d, n):
-    return a + (n-1) * d
+    return a + (n - 1) * d
 
-#Введення значень
-n = int(input("Введіть номер чену арифметичної прогресії: "))
-
-a=5
-d=2
-
-#Розрахунок
-nth_term = nth_term_arithmetic_progression(a, d, n)
-print(f"{n}-й член арифметичної прогресії: {nth_term}")
+if __name__ == "__main__":
+    n = int(input("Введіть номер члена арифметичної прогресії: "))
+    a = 5
+    d = 2
+    nth_term = nth_term_arithmetic_progression(a, d, n)
+    print(f"{n}-й член арифметичної прогресії: {nth_term}")
